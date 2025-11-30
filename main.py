66a091fae181770f320b4b5d33927dd7af489103
Goshan0404 <chernihov.g@gmail.com>
2025-11-30 10:14:26 +0500

    it is working

--- main.py
+++ main.py
@@ -1,81 +1,139 @@
+import re
 from typing import List
 
-from fastapi import FastAPI
-from fastapi import FastAPI, UploadFile
-
-import torch
 import pandas as pd
+from fastapi.responses import FileResponse
+import torch
+from fastapi import FastAPI, UploadFile, HTTPException
 from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
+from pandas import DataFrame
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-tokenizer = AutoTokenizer.from_pretrained("./tokenizer",  repo_type="local_files")
-model = AutoModelForSequenceClassification.from_pretrained("./bert")
+from models.Label import Label
+from models.Review import Review
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-class Review(BaseModel):
-    text: str
-
-class Label(BaseModel):
-    label: str
-    confidence: float
-
-
-all_labels = ['Positive', 'Negative', 'Zalupa']
+model_path = './bert'
+tokenizer_path = './tokenizer'
 app = FastAPI()
 
-@app.post("/labels/file")
-def get_labels(file: UploadFile):
-    if not file:
-        return {"message": "No upload file sent"}
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_path,  repo_type="local_files")
+model = AutoModelForSequenceClassification.from_pretrained(model_path)
 
-    try:
-        df = pd.read_csv(file.file)
+index_to_label = {0: 'Нейтральный', 1: 'Положительный', 2: 'Отрицательный'}
 
 
-        # проверяем, что есть колонка text
-        if "text" not in df.columns:
-            return {"exception": "CSV must contain a 'text' column"}
+def clean_text(text):
+    text = re.sub(r"<[^>]+>", "", text)        # удаляем html
+    text = re.sub(r"\s+", " ", text).strip()   # нормализуем пробелы
+    text = re.sub(r"[\n\r\t]+", " ", text)
+    text = re.sub(r"[\x00-\x1F\x7F]+", " ", text)
+    text = re.sub(r"([^\w\s])\1{2,}", r"\1\1", text)
+    text = re.sub(r"[^\w\s.,!?:;\"'()\-…/]+", " ", text)
+    text = re.sub(r"(.)\1{2,}", r"\1", text)
+    text = re.sub(r"\s*n\s*", " ", text)
 
-        # получаем список строк
-        texts = df["text"].astype(str).tolist()
+    return text
 
-        # токенизация
-        inputs = tokenizer(
-            texts,
-            padding=True,
+def preprocess(texts, head=100, tail=100, max_length=300):
+    processed = []
+
+    for text in texts:
+        ids = tokenizer(text, add_special_tokens=False)["input_ids"]
+
+        if len(ids) > head + tail + 10:  # запас
+            head_tokens = ids[:head]
+            tail_tokens = ids[-tail:]
+            mid_token = tokenizer.sep_token_id
+            new_ids = head_tokens + [mid_token] + tail_tokens
+        else:
+            new_ids = ids
+
+        encoded = tokenizer.prepare_for_model(
+            new_ids,
             truncation=True,
-            return_tensors='pt'
+            padding='max_length',
+            max_length=max_length,
+            return_tensors=None
         )
-        with torch.no_grad():
-            outputs = model(**inputs)
-            max_indices = torch.argmax(outputs.logits, dim=1)
-            max_probs = torch.softmax(outputs.logits, dim=1).max(dim=1).values
-            res = [Label(label=all_labels[i], confidence=p.item()) for i, p in zip(max_indices, max_probs)]
 
-            return {"labels": res}
+        processed.append(encoded)
 
-    except Exception as e:
-        return {"exception": f"failed to read: {e}"}
+    # Склеиваем дикты в батч
+    batch = {
+        "input_ids": torch.tensor([p["input_ids"] for p in processed]),
+        "attention_mask": torch.tensor([p["attention_mask"] for p in processed])
+    }
+
+    return batch
 
 
 
-@app.post("/labels/string")
-def get_labels(reviews: List[Review]):
-    if len(reviews) == 0:
-        return {"exception": "empty reviews"}
+@app.get('/')
+def root():
+    return "Get out of here"
 
-    reviews_data = jsonable_encoder(reviews)
+@app.post("/labels")
+def get_labels(review: str):
+    # if len(reviews) == 0:
+    #     raise HTTPException(status_code=404, detail="Empty list")
+    # reviews_data = jsonable_encoder(reviews)
 
-    texts = [r['text'] for r in reviews_data]
-    inputs = tokenizer(texts,  padding=True, truncation=True, return_tensors='pt')
+    texts = [clean_text(review)]
+
+    inputs = preprocess(texts)
+
     with torch.no_grad():
         outputs = model(**inputs)
         max_indices = torch.argmax(outputs.logits, dim=1)
         max_probs = torch.softmax(outputs.logits, dim=1).max(dim=1).values
-        res = [Label(label=all_labels[i], confidence=p.item()) for i, p in zip(max_indices, max_probs)]
+        df = DataFrame()
+        df['labels'] = pd.Series(max_indices.cpu().numpy()).map(index_to_label)
+        df['text'] = review
+        df['confidence'] = max_probs
+        path = 'predictions.csv'
+        df.to_csv(path)
 
-        return {"labels": res}
+        return FileResponse(path=path)
+
+
+@app.post("/labels/file")
+def get_labels(file: UploadFile):
+    if not file:
+        raise HTTPException(status_code=404, detail="File is not uploaded")
+
+    try:
+        import csv
+        df = pd.read_csv(
+            file.file,
+            sep=",")
+
+        if "text" not in df.columns:
+            return {"exception": "Csv file lacks of text column"}
+
+        # Чистим тексты
+        df["text"] = df["text"].astype(str).apply(clean_text)
+
+        # Токенизация пачкой
+        inputs = preprocess(df['text'].tolist())
+
+        with torch.no_grad():
+            outputs = model(**inputs)
+            max_indices = torch.argmax(outputs.logits, dim=1)
+            max_probs = torch.softmax(outputs.logits, dim=1).max(dim=1).values
+
+        df['labels'] = [index_to_label[int(i)] for i in max_indices]
+        df['confidence'] = max_probs.cpu().numpy()
+
+        path = 'predictions.csv'
+        df.to_csv(path, index=False)
+
+        return FileResponse(path=path)
+
+    except Exception as e:
+        return {"exception": f"failed to read: {e}"}
+
 
 
 
